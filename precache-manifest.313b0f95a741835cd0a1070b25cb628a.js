248974d4d9dc82cb7af7d08150a3c15108d35c08
Neeharika V <neeharules@gmail.com>
2020-08-22 02:00:43 -0700

    Updates

---
+++ precache-manifest.313b0f95a741835cd0a1070b25cb628a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1e5f24405922d08ae9331e4cba5135",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d13c61f852703dd6833",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "a804481cfdd25a6d5b3f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d13c61f852703dd6833",
+    "url": "/static/js/2.916096ba.chunk.js"
+  },
+  {
+    "revision": "d3031346469a8a2ef527f1ad705ea3b7",
+    "url": "/static/js/2.916096ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a804481cfdd25a6d5b3f",
+    "url": "/static/js/main.27cec0c5.chunk.js"
+  },
+  {
+    "revision": "e772e3ee0db6f9829b44",
+    "url": "/static/js/runtime-main.fdaf22cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
