56db9e1b2fe38f1283abad8da8329dfa50565d15
Neeharika V <neeharules@gmail.com>
2020-08-22 01:25:34 -0700

    add build folder

---
+++ build/precache-manifest.9a3a7456bec2a73739cf8721af74e10e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f985d0737938809293ed6ba8384cc19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a9e436ea9d88d580d6c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "000dcbc94d9cb4d62514",
+    "url": "/static/js/2.c2ac23dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c2ac23dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a9e436ea9d88d580d6c",
+    "url": "/static/js/main.510d915c.chunk.js"
+  },
+  {
+    "revision": "e772e3ee0db6f9829b44",
+    "url": "/static/js/runtime-main.fdaf22cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
